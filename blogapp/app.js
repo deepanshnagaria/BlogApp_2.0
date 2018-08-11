df818de6312ebd0d8ea77f992f8b939f79ab17ea
unknown <deepansh15111999@gmail.com>
2018-08-12 03:37:20 +0530

    Blog_app_2.0

--- blogapp/app.js
+++ blogapp/app.js
@@ -1,7 +1,9 @@
 var e= require("Express"),bodyparser=require("body-parser"),mongoose=require("mongoose"),methodOverride=require("method-override");
 var app=e();
+var passport= require("passport"),LocalStrategy= require("passport-local"),passportLocalMongoose= require("passport-local-mongoose");
+var User= require("./models/user");
 
-mongoose.connect("mongodb://localhost/blog_app");
+mongoose.connect("mongodb://localhost:27017/blog_app", { useNewUrlParser: true });
 app.set("view engine","ejs");
 app.use(e.static("public"));
 app.use(bodyparser.urlencoded({extended:true}));
@@ -16,7 +18,35 @@ var b=new mongoose.Schema({
 		default: Date.now
 	}
 });
+
+app.use(require("express-session")({
+	secret: "I,Deepansh Nagaria am the best developer in IIT Roorkee and I made this site for ARIES IITR",
+	resave: false,
+	saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+passport.use(new LocalStrategy(User.authenticate()));
+
+var m=new mongoose.Schema({
+	topic: String,
+	name: String,
+	body: String,
+	date: {
+		type:Date,
+		default: Date.now
+	}
+});
 var Blog=mongoose.model("Blog",b);
+var Message=mongoose.model("Message",m);
+app.use(function(req,res,next){
+	res.locals.currentUser=req.user;
+	next();
+})
 
 app.get("/blogs",function(req,res){
 	Blog.find({},function(err,blogs){
@@ -26,10 +56,13 @@ app.get("/blogs",function(req,res){
 	});	
 });
 app.get("/",function(req,res){
-	res.render("welcome_page");
+	res.render("welcome_page",{currentUser:req.user});
+})
+app.get("/secret", isLoggedIn,function(req,res){
+	res.send("I,Deepansh Nagaria am the best developer in IIT Roorkee and I made this site for ARIES IITR");
 })
 
-app.get("/new_blog",function(req,res){
+app.get("/new_blog",isLoggedIn,function(req,res){
 	res.render("new_blog");
 })
 
@@ -41,7 +74,7 @@ app.post("/blogs",function(req,res){
 	})
 })
 
-app.get("/blogs/:blog_id/edit",function(req,res){
+app.get("/blogs/:blog_id/edit",isLoggedIn,function(req,res){
 	Blog.findById(req.params.blog_id,function(err,foundblog){
 		if(!err&&foundblog)
 			res.render("Editing_blog",{foundblog:foundblog});
@@ -66,7 +99,7 @@ app.get("/blogs/:_id",function(req,res){
 })
 
 
-app.delete("/blogs/:id",function(req,res){
+app.delete("/blogs/:id",isLoggedIn,function(req,res){
 	Blog.findByIdAndRemove(req.params.id,function(err){
 		if(err)
 			res.redirect("/blogs/"+req.params.id);
@@ -74,6 +107,68 @@ app.delete("/blogs/:id",function(req,res){
 			res.redirect("/blogs");
 	})
 })
+
+app.post("/messages",function(req,res){
+	//res.send(req.body.message);
+	Message.create(req.body.message,function(err,newblog){
+		res.redirect("/thanks");
+	})
+})
+
+app.get("/messages",isLoggedIn,function(req,res){
+	Message.find({},function(err,messages){
+		res.render("message_dis",{messages:messages});
+	})
+})
+app.get("/thanks",function(req,res){
+	res.render("thanks");
+})
+
+
+app.get("/new_messages",function(req,res){
+	res.render("message");
+})
+
+//========================================================
+//========================================================
+//Auth
+//========================================================
+//========================================================
+app.get("/register",function(req,res){
+	res.render("register");
+})
+app.post("/register",function(req,res){
+	User.register(new User({username:req.body.username}),req.body.password,function(err,user){
+		if(err){
+			console.log(err);
+			//console.log(req.body.username);
+			return res.render('register');
+		}
+		passport.authenticate("local")(req,res,function(){
+			res.redirect("/blogs");
+		})
+	})
+})
+app.get("/login", function(req,res){
+	res.render("login");
+})
+app.post("/login",passport.authenticate("local",{
+	successRedirect:"/blogs",
+	failureRedirect:"/login"}),function(req,res){
+
+})
+app.get("/logout",function(req,res){
+	req.logout();
+	res.redirect("/blogs");
+})
+function isLoggedIn(req,res,next){
+	//console.log(req.isAuthenticated());
+	if(req.isAuthenticated()){
+		return next();
+	}
+	else
+		res.redirect("/login");
+}
 
 app.listen(3000,function(){
 	console.log("Running");
